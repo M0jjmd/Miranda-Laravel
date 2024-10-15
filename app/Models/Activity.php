d300283bfd70e3951a4d29d6767f7f075d1d2cac
M0jjmd <moamine517@gmail.com>
2024-10-15 16:50:28 +0200

    Added fillable propertie on activity model

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Activity extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'type',
+        'user_id',
+        'datetime',
+        'paid',
+        'notes',
+        'satisfaction',
+    ];
 }
