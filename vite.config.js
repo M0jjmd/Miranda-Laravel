51389b736810465f41cb9a93adc8baf022c744ef
root <root@ip-172-31-44-168.eu-west-3.compute.internal>
2024-11-07 14:53:46 +0000

    Changes to work in server

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,19 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+		'resources/scss/pages/index.scss',
+		'resources/scss/pages/about.scss',
+		'resources/scss/pages/contact.scss',
+		'resources/scss/pages/roomDetails.scss',
+		'resources/scss/pages/rooms.scss',
+		'resources/scss/pages/offers.scss',
                 'resources/css/app.css',
+		'resources/scss/app.scss',
                 'resources/js/app.js',
-                'resources/scss/styles.scss',
+		'resources/js/formatDates.js',
+		'resources/js/numbered-swiper.js',
+		'resources/js/openMenu.js',
+		'resources/js/swiper.js',
             ],
             refresh: true,
         }),
